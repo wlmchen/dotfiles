2b7498184bf8f8284da0ddf02e3772222642f14c
pryme-svg <edoc.www@gmail.com>
2021-05-24 16:29:19 -0700

    add polybar scripts

---
+++ .config/polybar/launch.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Terminate already running bar instances
+killall -q polybar
+
+# Wait until the processes have been shut down
+while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
+
+# Launch Polybar, using default config location ~/.config/polybar/config
+# Launch bars by monitor names
+if type "xrandr"; then
+  for m in $(xrandr --listactivemonitors | grep -v Monitors | grep -oE '[^ ]+$'); do
+    polybar $m &
+  done
+fi
+
+echo "Polybar launched..."
+
